476b4934e90a650374cd645d59e23ba8a6676ce2
NadirYusifov <nadiryusifov448@gmail.com>
2026-01-31 23:52:19 +0400

    map added

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -3,8 +3,7 @@ import * as am4maps from "@amcharts/amcharts4/maps"
 import am4geodata_azerbaijanHigh from "@amcharts/amcharts4-geodata/azerbaijanHigh";
 import { regionData, type RegionData } from "../../data";
 
-
-export default function index() {
+export default function Map() {
   const map = am4core.create("chartdiv", am4maps.MapChart);
   map.geodata = am4geodata_azerbaijanHigh
   map.projection = new am4maps.projections.Mercator()
@@ -17,16 +16,16 @@ export default function index() {
   const polygonTemplate = polygonSeries.mapPolygons.template;
   polygonTemplate.tooltipText = "{name}";
   polygonTemplate.fill = am4core.color("#4285F4");
-  polygonTemplate.events.on("hit", function(e) {
+  polygonTemplate.events.on("hit", function (e) {
     // e.target.series.chart.zoomToMapObject(e.target)
     const data = e.target.dataItem.dataContext as RegionData
     const info = document.getElementById("info")
     if (info) {
-      info.innerHTML = "<h3>" + data.name +"</h3>"
-      info.innerHTML += "<p>" + "Sahə: " + data.field +"</p>"
-      info.innerHTML += "<p>" + "Şəhərlərin sayı: " + data.citynum +"</p>"
-      info.innerHTML += "<p>" + "Kənd sayı: " + data.regionnum +"</p>"
-      info.innerHTML += "<p>" + "Kənd şəhər mərkəzi: " + data.regioncentername +"</p>"
+      info.innerHTML = "<h3>" + data.name + "</h3>"
+      info.innerHTML += "<p>" + "Sahə: " + data.field + " km2" + "</p>"
+      info.innerHTML += "<p>" + "Şəhərlərin sayı: " + data.citynum + "</p>"
+      info.innerHTML += "<p>" + "Kənd sayı: " + data.regionnum + "</p>"
+      info.innerHTML += "<p>" + "Kənd şəhər mərkəzi: " + data.regioncentername + "</p>"
     } else {
       document.body.innerHTML = "<h3>No info element found</h3>"
     }
@@ -49,9 +48,12 @@ export default function index() {
 
   return (
     <>
-    <div id="chartdiv"></div>
-    <div id="info"></div>
+      <div id="chartdiv"></div>
+      <div className="container px-4">
+        <div id="info">
+        </div>
+      </div>
     </>
-    
+
   )
 }
