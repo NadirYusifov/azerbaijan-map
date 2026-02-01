bd21983483cc5fe036cb111885f27ac38c1c2cdf
NadirYusifov <nadiryusifov448@gmail.com>
2026-02-02 02:36:30 +0400

    feat(map): refactor map component to use React hooks and state management
    - Converted from class-based DOM manipulation to React functional component with hooks
    - Added useState for region data management and useRef for chart container
    - Wrapped map initialization in useEffect with proper cleanup (dispose map on unmount)
    - Replaced direct DOM manipulation with Info component integration
    - Added proper null checks and ref references for React rendering
    - Added zoom control and home button for better user interaction

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,56 +1,65 @@
+import Info from "../info";
+import { useEffect, useRef, useState } from "react";
 import * as am4core from "@amcharts/amcharts4/core"
 import * as am4maps from "@amcharts/amcharts4/maps"
-import am4geodata_azerbaijanHigh from "@amcharts/amcharts4-geodata/azerbaijanHigh";
 import { regionData, type RegionData } from "../../data";
+import am4geodata_azerbaijanHigh from "@amcharts/amcharts4-geodata/azerbaijanHigh";
 
-export default function Map() {
-  const map = am4core.create("chartdiv", am4maps.MapChart);
-  map.geodata = am4geodata_azerbaijanHigh
-  map.projection = new am4maps.projections.Mercator()
+export default function AzerbaijanMap() {
+  const [regiondata, setRegionData] = useState<RegionData | null>(null)
+  const charDivRef = useRef<HTMLDivElement>(null)
+  const chartInstanceRef = useRef<am4core.Container | null>(null)
 
-  const polygonSeries = new am4maps.MapPolygonSeries();
-  polygonSeries.useGeodata = true;
-  map.series.push(polygonSeries)
-  polygonSeries.data = regionData
+  useEffect(() => {
+    if (!charDivRef.current) return;
 
-  const polygonTemplate = polygonSeries.mapPolygons.template;
-  polygonTemplate.tooltipText = "{name}";
-  polygonTemplate.fill = am4core.color("#4285F4");
-  polygonTemplate.events.on("hit", function (e) {
-    // e.target.series.chart.zoomToMapObject(e.target)
-    const data = e.target.dataItem.dataContext as RegionData
-    const info = document.getElementById("info")
-    if (info) {
-      info.innerHTML = "<h3>" + data.name + "</h3>"
-      info.innerHTML += "<p>" + "Sahə: " + data.field + " km2" + "</p>"
-      info.innerHTML += "<p>" + "Şəhərlərin sayı: " + data.citynum + "</p>"
-      info.innerHTML += "<p>" + "Kənd sayı: " + data.regionnum + "</p>"
-      info.innerHTML += "<p>" + "Kənd şəhər mərkəzi: " + data.regioncentername + "</p>"
-    } else {
-      document.body.innerHTML = "<h3>No info element found</h3>"
-    }
-    // if (info) {
-    //   info.innerHTML += "<p>"+ data.field +"</p>"
-    // }
-    // const contents = info?.innerHTML
+    const map = am4core.create(charDivRef.current, am4maps.MapChart);
+    chartInstanceRef.current = map
 
-    console.log(data)
-    // console.log(info)
-    // console.log(contents)
+    map.geodata = am4geodata_azerbaijanHigh
+    map.projection = new am4maps.projections.Mercator()
+
+    const polygonSeries = new am4maps.MapPolygonSeries();
+    polygonSeries.useGeodata = true;
+    map.series.push(polygonSeries)
+    polygonSeries.data = regionData
+
+    const polygonTemplate = polygonSeries.mapPolygons.template;
+    polygonTemplate.tooltipText = "{name}";
+    polygonTemplate.fill = am4core.color("#4285F4");
+
+    polygonTemplate.events.on("hit", function (e) {
+      e.target.series.chart.zoomToMapObject(e.target)
+      const data = e.target.dataItem.dataContext as RegionData
+      setRegionData(data)
+    })
+
+    const button = map.chartContainer.createChild(am4core.Button);
+    button.padding(5, 5, 5, 5);
+    button.margin(5, 5, 5, 5)
+    button.align = "right"
+    button.events.on("hit", function () {
+      map.goHome()
+    })
+    button.icon = new am4core.Sprite()
+    button.icon.path = "M22.719 12A10.719 10.719 0 0 1 1.28 12h.838a9.916 9.916 0 1 0 1.373-5H8v1H2V2h1v4.2A10.71 10.71 0 0 1 22.719 12z"
 
     map.zoomControl = new am4maps.ZoomControl()
-  })
 
-  // map.zoomControl = new am4maps.ZoomControl()
+    const hoverstate = polygonTemplate.states.create("hover");
+    hoverstate.properties.fill = am4core.color("#FF0000");
 
-  const hoverstate = polygonTemplate.states.create("hover");
-  hoverstate.properties.fill = am4core.color("#FF0000");
+    return (() => {
+      map.dispose()
+    })
+  }, [])
 
   return (
     <>
-      <div id="chartdiv"></div>
+      <div ref={charDivRef} id="chartdiv" style={{ width: "100%", height: "50dvh" }}></div>
       <div className="container px-4">
         <div id="info">
+          <Info region={regiondata} />
         </div>
       </div>
     </>
