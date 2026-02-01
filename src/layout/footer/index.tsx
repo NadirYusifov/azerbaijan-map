64117dc5daf7a24c363e1e3f8ca02a2a8def59a6
NadirYusifov <nadiryusifov448@gmail.com>
2026-02-02 02:37:12 +0400

    added footer component

---
+++ src/layout/footer/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router'
+import GitHub from '../../ui/icon'
+
+export default function Footer() {
+    return (
+        <footer className='border-t border-neutral-200 py-5'>
+            <div className='container px-4 mx-auto flex items-center justify-center text-center'>
+                <Link to={"https://github.com/NadirYusifov"} rel="prefetch">
+                    <span><GitHub /></span>
+                </Link>
+            </div>
+        </footer>
+    )
+}
