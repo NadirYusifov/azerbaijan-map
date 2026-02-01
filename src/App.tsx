c80001c210f92a089089db759edd224c4bd87a21
NadirYusifov <nadiryusifov448@gmail.com>
2026-02-02 02:33:45 +0400

    Routing system added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import AzerbaijanMap from "../src/components/map/index";
-// import { regionData } from "./data";
-// import { regionData } from "./data";
-// import Azerbaijan from "/azerbaijan.svg"
-// import Azerbaijan from "/azerbaijanHigh.svg"
+import Footer from "./layout/footer";
+import Navbar from "./layout/navbar";
+import About from "./pages/about";
+import Home from "./pages/home";
+import { Route, Routes } from "react-router";
 
 export default function App() {
   return (
-    <section>
-      {/* <img className="land" src={Azerbaijan} alt="Azerbaijan Map" /> */}
-      <AzerbaijanMap />
-    </section>
+    <>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="haqqimizda" element={<About />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
