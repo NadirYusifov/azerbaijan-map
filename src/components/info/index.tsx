80552d3c9f833066ac8901a5080890b22b7f91ec
NadirYusifov <nadiryusifov448@gmail.com>
2026-02-02 17:39:00 +0400

    new content added "Məlumat yoxdur və ya məlumat tapılmadı"

--- src/components/info/index.tsx
+++ src/components/info/index.tsx
@@ -10,11 +10,11 @@ export default function Info({ region }: DataProps) {
             {region ?
                 <article className="space-y-1">
 
-                    <h1 className="font-bold text-3xl">{region?.name || "Məlumat yoxdur"}</h1>
-                    <p className="map-content"><span>Sahə:</span> {region?.field || "Məlumat yoxdur"}</p>
-                    <p className="map-content"><span>Yaranma tarixi:</span> {region?.date || "Məlumat yoxdur"}</p>
-                    <p className="map-content"><span>Kənd sayı:</span> {region?.regionnum || "Məlumat yoxdur"}</p>
-                    <p className="map-content"><span>Kənd şəhər mərkəzi:</span> {region?.regioncentername || "Məlumat yoxdur"}</p>
+                    <h1 className="font-bold text-3xl">{region?.name || "Məlumat yoxdur və ya tapılmadı"}</h1>
+                    <p className="map-content"><span>Sahə:</span> {region.field || "Məlumat yoxdur və ya tapılmadı"}</p>
+                    <p className="map-content"><span>Yaranma tarixi:</span> {region.date || "Məlumat yoxdur və ya tapılmadı"}</p>
+                    <p className="map-content"><span>Kənd sayı:</span> {region.regionnum || "Məlumat yoxdur və ya tapılmadı"}</p>
+                    <p className="map-content"><span>Kənd şəhər mərkəzi:</span> {region.regioncentername || "Məlumat yoxdur və ya tapılmadı"}</p>
                 </article> : <h1 className="text-3xl">Məlumat almaq üçün xəritə üzərindən bir rayon və yaxud bir şəhər seçin.</h1>
             }
         </div>
