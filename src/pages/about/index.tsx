5ada33000476404cb2cac433094476f7cdc582bd
NadirYusifov <nadiryusifov448@gmail.com>
2026-02-02 23:35:13 +0400

    article tag inside about-referance classname and li tag added wrap-break-word style

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -16,15 +16,17 @@ export default function About() {
                             <span className="text-red-500">Diqqət:</span> Bəzi bölgələr haqqında məlumat düzgün verilməyə bilər və ya heç yazılmaya bilər. Əgər əlinizdə hazır məlumatlar varsa <Link className="text-sky-500 underline" to={"https://github.com/NadirYusifov/azerbaijan-map/issues"}>GitHub Issues</Link> hissəsində yaza bilərsiz.
                         </p>
                     </article>
-                    <p className="about-referance font-bold">
-                        Referanslar:
-                    </p>
-                    <ul>
-                        <li>Rayon məlumatları: <Link className="text-sky-500 underline" to={"https://republic.preslib.az/az_a1.html"}>https://republic.preslib.az/az_a1.html</Link></li>
-                        <li>Gədəbəy (Başkənd): <Link className="text-sky-500 underline" to={"https://az.wikipedia.org/wiki/Ba%C5%9Fk%C9%99nd_(G%C9%99d%C9%99b%C9%99y)"}>https://az.wikipedia.org/wiki/Ba%C5%9Fk%C9%99nd_(G%C9%99d%C9%99b%C9%99y)</Link></li>
-                        <li>Naxçıvan (şəhər): <Link className="text-sky-500 underline" to={"https://az.wikipedia.org/wiki/Nax%C3%A7%C4%B1van_(%C5%9F%C9%99h%C9%99r)"}>https://az.wikipedia.org/wiki/Nax%C3%A7%C4%B1van_(%C5%9F%C9%99h%C9%99r)</Link></li>
-                        <li>Rayon məlumatları: <Link className="text-sky-500 underline" to={"https://files.preslib.az/projects/azerbaijan/gl2.pdf#page138"}>https://files.preslib.az/projects/azerbaijan/gl2.pdf#page138</Link></li>
-                    </ul>
+                    <article className="about-referance">
+                        <p className="font-bold">
+                            Referanslar:
+                        </p>
+                        <ul>
+                            <li>Rayon məlumatları: <Link className="text-sky-500 underline wrap-break-word" to={"https://republic.preslib.az/az_a1.html"}>https://files.preslib.az/projects/azerbaijan/gl2.pdf#page138</Link></li>
+                            <li>Gədəbəy (Başkənd): <Link className="text-sky-500 underline wrap-break-word" to={"https://az.wikipedia.org/wiki/Ba%C5%9Fk%C9%99nd_(G%C9%99d%C9%99b%C9%99y)"}>https://az.wikipedia.org/wiki/Ba%C5%9Fk%C9%99nd_(G%C9%99d%C9%99b%C9%99y)</Link></li>
+                            <li>Naxçıvan (şəhər): <Link className="text-sky-500 underline wrap-break-word" to={"https://az.wikipedia.org/wiki/Nax%C3%A7%C4%B1van_(%C5%9F%C9%99h%C9%99r)"}>https://az.wikipedia.org/wiki/Nax%C3%A7%C4%B1van_(%C5%9F%C9%99h%C9%99r)</Link></li>
+                            <li>Rayon məlumatları: <Link className="text-sky-500 underline wrap-break-word" to={"https://files.preslib.az/projects/azerbaijan/gl2.pdf#page138"}>https://files.preslib.az/projects/azerbaijan/gl2.pdf</Link></li>
+                        </ul>
+                    </article>
                 </div>
             </div>
         </section >
