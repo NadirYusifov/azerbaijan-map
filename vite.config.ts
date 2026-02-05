6244e7c8ee868e2b23f82ff095a6cf4c1a987409
NadirYusifov <nadiryusifov448@gmail.com>
2026-02-06 01:33:01 +0400

    add: manualchunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+          "amcharts-vendor": [
+            "@amcharts/amcharts4/core",
+            "@amcharts/amcharts4/maps",
+            "@amcharts/amcharts4-geodata/azerbaijanHigh",
+          ],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 800,
+    minify: "esbuild",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+  },
+});
