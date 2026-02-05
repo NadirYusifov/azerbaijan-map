8341e8bf7cecf49ffdd23c9ba676ad777cc27cf7
NadirYusifov <nadiryusifov448@gmail.com>
2026-02-06 01:34:06 +0400

    add: loading component added and some style

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,8 @@
+export const Loading = () => {
+  return (
+    <section className="h-dvh flex flex-col space-y-3 items-center justify-center text-center">
+      <h1 className="text-5xl text-sky-500">Yüklənir...</h1>
+      <p className="text-2xl">Zəhmət olmasa gözləyin.</p>
+    </section>
+  );
+};
